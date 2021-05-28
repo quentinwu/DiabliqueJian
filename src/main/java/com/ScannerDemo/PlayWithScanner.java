7d96822e875897080d365e7e88ea7ca689cf98dc
quentin woo <quentin woo>
2021-05-28 00:15:32 -0400

    This commit happened after iss53 was deleted

--- src/main/java/com/ScannerDemo/PlayWithScanner.java
+++ src/main/java/com/ScannerDemo/PlayWithScanner.java
@@ -14,6 +14,7 @@ public class PlayWithScanner {
                 break;
             }
             //operate
+            // This line is added after iss53 branch is deleted
             s1 = "I've got " + s1;
             System.out.println(s1);
         }
