a173d26ff1b784bc02a928adf4e467b1958097bf
quentin woo <quentin woo>
2021-05-28 01:03:35 -0400

    Attempt to resolve

--- src/main/java/com/ScannerDemo/PlayWithScanner.java
+++ src/main/java/com/ScannerDemo/PlayWithScanner.java
@@ -8,14 +8,16 @@ import java.util.Scanner;
 public class PlayWithScanner {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
+        int cnt = 0;
         while(sc.hasNext()) {
             String s1 = sc.next();
             if(s1.equals("quit") || s1.equals("exit")) {
                 break;
             }
             //operate
-            s1 = "Got " + s1;
+            s1 = "Got " + s1 + " " + cnt;
             System.out.println(s1);
+            cnt ++;
         }
         System.out.println("Program is done.");
 
